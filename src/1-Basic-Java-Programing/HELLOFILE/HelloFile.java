59df636b90c0cfe0677fb16b451139669d28f05d
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2024-09-05 06:14:33 +0700

    Add HELLOFILE

---
+++ src/1-Basic-Java-Programing/HELLOFILE/HelloFile.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of HelloFile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HelloFile {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("Hello.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e){
+        }
+    }
+}

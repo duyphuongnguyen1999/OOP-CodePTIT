5c12ba403c75b3c2e932f4241324f86e3783a899
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2024-10-28 19:52:52 +0700

    Add J01002

--- src/1-Basic-Java-Programing/J01002/J01002.java
+++ src/1-Basic-Java-Programing/J01002/J01002.java
@@ -4,9 +4,13 @@ public class J01002 {
     public static void main(String arg[]){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        while (n > 10) {
+            n = in.nextInt();
+        }
         while(n-- > 0) {
-            int N = in.nextInt();
+            long N = in.nextInt();  
             System.out.println((N+1)*N/2);
         }
+        in.close();
     }
 }

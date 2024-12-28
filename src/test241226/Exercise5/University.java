b5aab5fdf90bb65030f09eb94e2cc1c089bd32fa
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2024-12-28 23:00:13 +0700

    Add University.java

---
+++ src/test241226/Exercise5/University.java
@@ -0,0 +1,13 @@
+package test241226.Exercise5;
+
+public class University {
+    private String universityShortName;
+    private String universityName;
+
+    public University(String universityShortName, String universityName) {
+        this.universityShortName = universityShortName;
+        this.universityName = universityName;
+    }
+
+    public String getUniversityName() { return universityName; }
+}
